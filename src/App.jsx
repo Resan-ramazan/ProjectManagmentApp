a83f9eddcf48a48e8165927b77c6d005839bb7e5
Ramazan Alkoyun <ramazan.resan@gmail.com>
2025-01-12 23:15:05 +0100

    Add delete functionality to SelectedProject component

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,18 @@ function App() {
     });
   };
 
+  const handleDeleteProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  };
+
   const handleStartAddProject = () => {
     setProjectState((prevState) => {
       return {
@@ -57,7 +69,9 @@ function App() {
     (project) => project.id === projectState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectState.selectedProjectId === null) {
     content = <NewProject onCancel={handleCancel} onAdd={handleAddProject} />;
